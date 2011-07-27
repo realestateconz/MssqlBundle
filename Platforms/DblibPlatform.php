959265a0a30bb31d99acc710f9bcdfdb2838e35d
pasinter <ken@webplanet.co.nz>
2011-07-28 10:22:06 +1200

    applied patch for SELECT DISTINCT TOP bug

--- Platforms/DblibPlatform.php
+++ Platforms/DblibPlatform.php
@@ -59,7 +59,17 @@ class DblibPlatform extends MsSqlPlatform
             }
 
             if ($offset == 0) {
-                $query = preg_replace('/^SELECT\s/i', 'SELECT TOP ' . $count . ' ', $query);
+                // SELECT TOP DISTINCT does not work with mssql
+                if(preg_match('#^SELECT\s+DISTINCT#i', $query) !== false) {
+                    $query = preg_replace('/^SELECT\s+DISTINCT\s/i', 'SELECT DISTINCT TOP ' . $count . ' ', $query);
+                    echo $query;
+                } else {
+                    echo $query;
+                    $query = preg_replace('/^SELECT\s/i', 'SELECT TOP ' . $count . ' ', $query);
+                    echo $query;exit;
+                }
+
+                
             } else {
                 $orderby = stristr($query, 'ORDER BY');
 
