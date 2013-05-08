b75db85a5e65db103d510954d8da2c3c70b0000c
pasinter <ken.golovin@gmail.com>
2013-05-09 09:38:22 +1200

    update platform

--- Platforms/DblibPlatform.php
+++ Platforms/DblibPlatform.php
@@ -25,7 +25,7 @@ use Doctrine\DBAL\DBALException,
     Doctrine\DBAL\Schema\TableDiff;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Platforms\MsSqlPlatform;
+use Doctrine\DBAL\Platforms\SQLServerPlatform;
 
 /**
  * The DblibPlatform provides the behavior, features and SQL dialect of the
@@ -36,7 +36,7 @@ use Doctrine\DBAL\Platforms\MsSqlPlatform;
  * @author Roman Borschel <roman@code-factory.org>
  * @author Benjamin Eberlei <kontakt@beberlei.de>
  */
-class DblibPlatform extends MsSqlPlatform
+class DblibPlatform extends SQLServerPlatform
 {
 
     /**
